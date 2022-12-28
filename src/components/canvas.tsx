fa66b7bfdd6bb95b61fbcd3effa28462c9dd1081
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-28 20:40:54 +0000

    Declare state for the canvas

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+
 export default function Canvas() {
+  const [state, setState] = useState({
+    lines: [], // array of lines drawn on the canvas
+    cursor: { x: 0, y: 0 }, // current position of the cursor
+    color: '#000000', // color of the lines
+    thickness: 2, // thickness of the lines
+  });
+
   return (
     <div className="canvas">
       <p>Canvas</p>
