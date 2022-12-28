67681420c270044194a0d29f3bfddbe19a5d5454
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-28 21:30:24 +0000

    Add github logo

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+
 export default function NavBar() {
   return (
     <nav>
       <h1>Etch A Sketch</h1>
+      <div className='nav-icons-wrapper'>
+        <FontAwesomeIcon icon={faSquareGithub} />
+      </div>
     </nav>
-  )
+  );
 }
