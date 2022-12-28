7bb45ce73ae7f32765e9b5d9da33cf47de69535f
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-28 21:36:45 +0000

    Create container around logo

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -4,7 +4,9 @@ import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 export default function NavBar() {
   return (
     <nav>
-      <h1>Etch A Sketch</h1>
+      <div className="logo-container">
+        <h1>Etch A Sketch</h1>
+      </div>
       <div className='nav-icons-wrapper'>
         <FontAwesomeIcon icon={faSquareGithub} />
       </div>
