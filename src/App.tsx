170aede4611d76ac1bbe4937ae6aeeb4951cbb2b
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-28 20:33:46 +0000

    Start canvas and remove react.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Canvas from './components/canvas'
 
 function App() {
   return (
     <div className="App">
-      <h1>React TypeScript Template</h1>
+      <h1>Etch A Sketch</h1>
+      <Canvas />
     </div>
   )
 }
---
+++ src/components/canvas.tsx
@@ -0,0 +1,7 @@
+export default function Canvas() {
+  return (
+    <div className="canvas">
+      <p>Canvas</p>
+    </div>
+  )
+}
