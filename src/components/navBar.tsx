d2cbfb380d2a68c2e57055e9f5be9f666dd1d9b6
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-29 16:51:09 +0000

    Improve nav alignment and stretch app to edges

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -4,6 +4,8 @@ import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 export default function NavBar() {
   return (
     <nav>
+      <div className="nav-left">
+      </div>
       <div className="logo-container">
         <h1>Etch A Sketch</h1>
       </div>
