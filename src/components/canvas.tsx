f33b108cb988e5a44a25e6be5504539d2d9ff19c
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-02-11 18:50:47 +0000

    Define canvas state type

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export default function Canvas() {
-  const [state, setState] = useState({
+  const [state, setState] = useState<canvasState>({
     lines: [], // array of lines drawn on the canvas
     cursor: { x: 0, y: 0 }, // current position of the cursor
     color: '#000000', // color of the lines
@@ -15,4 +15,11 @@ export default function Canvas() {
       </canvas>
     </div>
   )
+}
+
+type canvasState = {
+  lines: Array<{ x: number, y: number }>,
+  cursor: { x: number, y: number },
+  color: string,
+  thickness: number,
 }
